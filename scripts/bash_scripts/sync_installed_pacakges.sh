df75b54f40ebc5aa53f7f5ac1fd695cba10d71a2
benluft <benluft8@gmail.com>
2022-03-19 12:41:55 -0600

    Daily update

---
+++ scripts/bash_scripts/sync_installed_pacakges.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ~/linux_environment/
+if [[ 'git status --porcelain' ]] then
+   /usr/bin/git add -A 
+   /usr/bin/git commit -am "Daily update"
+   /usr/bin/git push -u origin master
+fi
