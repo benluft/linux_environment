4122d72176a268cacbedd088fbc6be85d995807a
benluft <benluft8@gmail.com>
2022-03-19 13:46:02 -0600

    Daily update 1

--- scripts/bash_scripts/sync_installed_packages.sh
+++ scripts/bash_scripts/sync_installed_packages.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [[ 'git status --porcelain' ]]; then
-   git -C ~/linux_environment/ add -A 
-   git -C ~/linux_environment/ commit -m "Daily update 1"
-   git -C ~/linux_environment/ push origin main > text.txt
-fi
+#if [[ 'git status --porcelain' ]]; then
+#   git -C ~/linux_environment/ add -A 
+#   git -C ~/linux_environment/ commit -m "Daily update 1"
+#   git -C ~/linux_environment/ push origin main > text.txt
+#fi
+
+git -C ~/linux_environment/ add -A 
+git -C ~/linux_environment/ commit -m "Daily update 1"
+git -C ~/linux_environment/ push origin main > text.txt
