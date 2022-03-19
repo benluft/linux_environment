b04f4ffe824824da0d6ae76696b28fc143aad4b6
benluft <benluft8@gmail.com>
2022-03-19 14:29:10 -0600

    Minor comments, pulling before pushing

--- scripts/bash_scripts/sync_installed_packages.sh
+++ scripts/bash_scripts/sync_installed_packages.sh
@@ -8,9 +8,11 @@ fi
 
 cd ~/linux_environment
 
+# Always pull just in case changed somewhere else
+git pull
+
 if [[ 'git status --porcelain ./setup/module_installs/*.txt' ]]; then
-   git add ./setup/module_installs 
+   git add ./setup/module_installs
    git commit -m "Daily update module installs $(date +%F)"
    git push
 fi
-
