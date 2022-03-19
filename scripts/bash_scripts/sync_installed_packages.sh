0e9053e9f5e0f31ce300f075b7ab11793e127cbe
benluft <benluft8@gmail.com>
2022-03-19 13:41:01 -0600

    Daily update 1

--- scripts/bash_scripts/sync_installed_packages.sh
+++ scripts/bash_scripts/sync_installed_packages.sh
@@ -3,6 +3,6 @@
 cd ~/linux_environment/
 if [[ 'git status --porcelain' ]]; then
    git add -A 
-   git commit -am "Daily update 1"
+   git commit -m "Daily update 1"
    git push origin main > text.txt
 fi
